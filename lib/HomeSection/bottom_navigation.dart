51387b4a5addae97a333c35972c06f0f4e1ab435
nuyiwa908 <maoja37@gmail.com>
2022-04-05 20:15:04 +0100

    Started with card details screen

--- lib/HomeSection/bottom_navigation.dart
+++ lib/HomeSection/bottom_navigation.dart
@@ -3,6 +3,7 @@
 import 'package:flutter/material.dart';
 import 'package:one_wallet/HomeSection/my_cards_screen.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class BottomNavigationScreen extends StatefulWidget {
   const BottomNavigationScreen({Key? key}) : super(key: key);
@@ -28,21 +29,57 @@ class _BottomNavigationScreenState extends State<BottomNavigationScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: _screens[_selectedIndex],
+      //create a floating action bar with floatingActionButtonLocation center docked and icon : Icons.add
+      floatingActionButton: Container(
+        height: 56,
+        width: 56,
+        child: FittedBox(
+          child: FloatingActionButton(
+            onPressed: () {},
+            child: Icon(
+              Iconsax.add,
+              color: Colors.white,
+            ),
+            backgroundColor: Color(0xff02003D),
+          ),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       bottomNavigationBar: BottomNavigationBar(
+        elevation: 0,
+        backgroundColor: Color(0xffFAFAFA),
         items: <BottomNavigationBarItem>[
           BottomNavigationBarItem(
             label: 'My cards',
-            icon: Icon(
-              Iconsax.cards,
-              color:Color(0xffAAA8BD),
-            ),
+            icon: _selectedIndex == 0
+                ? Padding(
+                    padding: const EdgeInsets.only(bottom: 4),
+                    child:
+                        SvgPicture.asset('assets/bottom_navbar_card_blue.svg'),
+                  )
+                : Padding(
+                    padding: const EdgeInsets.only(bottom: 4),
+                    child: Icon(
+                      Iconsax.cards,
+                      color: Color(0xffAAA8BD),
+                    ),
+                  ),
           ),
           BottomNavigationBarItem(
             label: 'Settings',
-            icon: Icon(
-              Iconsax.setting,
-              color: Color(0xffAAA8BD),
-            ),
+            icon: _selectedIndex == 1
+                ? Padding(
+                    padding: const EdgeInsets.only(bottom: 4),
+                    child: SvgPicture.asset(
+                        'assets/bottom_navbar_setting_blue.svg'),
+                  )
+                : Padding(
+                    padding: const EdgeInsets.only(bottom: 4),
+                    child: Icon(
+                      Iconsax.setting,
+                      color: Color(0xffAAA8BD),
+                    ),
+                  ),
           ),
         ],
         currentIndex: _selectedIndex,
